5a8a8e9d199c56c09b818b88b2193ab39df63348
Gabriel Tanod <gabrielctanod@gmail.com>
2025-07-20 20:55:41 +0700

    Initial Commit

---
+++ FDNavigation/FDNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDNavigationApp.swift
+//  FDNavigation
+//
+//  Created by Gabriel Tanod on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FDNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
